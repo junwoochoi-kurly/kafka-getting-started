cd0f45965ad4c613fce0d607f8aa9e6c0a551528
junwoochoi-kurly <junwoo.choi@kurlycorp.com>
2024-08-28 09:48:59 +0900

    feat : project init

---
+++ range-assignor/src/main/java/com/example/rangeassignor/RangeAssignorApplication.java
@@ -0,0 +1,13 @@
+package com.example.rangeassignor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangeAssignorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RangeAssignorApplication.class, args);
+    }
+
+}
